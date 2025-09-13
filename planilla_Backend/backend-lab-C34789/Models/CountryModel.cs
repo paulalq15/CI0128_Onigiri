205ee7ef844831960edbd1d089fd2fa34a6bf403
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-13 02:23:10 -0600

    Estructura inicial del proyecto?

---
+++ planilla_Backend/backend-lab-C34789/Models/CountryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_lab_C34789.Models
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string Language {  get; set; }
+    }
+}
