615e3f60a8699aaaed9c3e67e7669577db9a664a
paulalq15 <paulalq15@gmail.com>
2025-09-13 10:19:51 -0600

    Agregando componente de Header con bootstrap icons y Footer básico, para utilizarlo en el home page

---
+++ planilla_frontend/src/components/FooterComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="py-3 my-4"> 
+        <p class="text-center text-body-secondary">© 2025 Todos los derechos reservados - Onigiri Intelligent Solutions</p> 
+    </footer>
+</template>
+
+<script>
+    export default {
+        setup() {
+            return {}
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- planilla_frontend/src/components/HomePage.vue
+++ planilla_frontend/src/components/HomePage.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
+    <HeaderComp />
+    <main>
         <h1>Home Page</h1>
-    </div>
+    </main>
+    <FooterComp />
 </template>
 
 <script>
+    import HeaderComp from './HeaderComp.vue';
+    import FooterComp from './FooterComp.vue';
     export default {
-        setup() {
-            return {}
-        },
-    };
+        components: {
+        HeaderComp,
+        FooterComp
+        }
+    }
+
 </script>
 
 <style lang="scss" scoped>
