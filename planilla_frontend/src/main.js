907c651bcb58600fa6eb12d9b75686a4cb2d6ffe
paulalq15 <paulalq15@gmail.com>
2025-09-15 10:48:09 -0600

    Agregando nuevo componente para crear compañía, aplicando .gitignore y eliminar archivos no deseados del repo

--- planilla_frontend/src/main.js
+++ planilla_frontend/src/main.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './components/HomePage.vue';
+import CreateCompany from './components/CreateCompany.vue';
 
 const router = createRouter ({
     history: createWebHistory(),
     routes: [
         {path:"/", name: "Home Page", component: HomePage},
+        {path:"/CrearEmpresa", name: "Crear Empresa", component: CreateCompany},
     ],
 });
 
