004a42e43233f5de9688c258507db639b94df06e
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-20 11:54:08 -0600

    Creacion clase Persona

---
+++ planilla_Backend/Planilla_Backend/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Planilla_Backend.Models
+{
+    public class Person
+    {
+        public int IdPersona { get; set; }
+        public string Cedula { get; set; } = "";
+        public string Nombre1 { get; set; } = "";
+        public string? Nombre2 { get; set; }
+        public string Apellido1 { get; set; } = "";
+        public string? Apellido2 { get; set; }
+        public string? Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string TipoPersona { get; set; } = "";
+    }
+}
