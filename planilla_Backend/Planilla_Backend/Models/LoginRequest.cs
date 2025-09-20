7bfba41a6868b3a21359f6f59864a0a2bb0cd5aa
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-20 11:56:25 -0600

    clases para enviar y recibit datos de la base de datos

---
+++ planilla_Backend/Planilla_Backend/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Planilla_Backend.Models
+{
+    public class LoginRequest
+    {
+        public string Correo { get; set; } = "";
+        public string Contrasena { get; set; } = "";
+    }
+}
---
+++ planilla_Backend/Planilla_Backend/Models/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Planilla_Backend.Models
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = "";
+        public int? IdUsuario { get; set; }
+        public int? IdPersona { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? TipoPersona { get; set; }
+        public string? Correo { get; set; }
+    }
+}
