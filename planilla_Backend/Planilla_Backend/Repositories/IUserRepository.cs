85cfcb520382f7b7c9e615be220bdf25887ecc5b
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-20 11:55:04 -0600

    creacion de objetos para la tranferir datos desde y hacia la base de datos

---
+++ planilla_Backend/Planilla_Backend/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Planilla_Backend.Models;
+
+namespace Planilla_Backend.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetActiveEmailAsync(string correo);
+    }
+}
---
+++ planilla_Backend/Planilla_Backend/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Planilla_Backend.Models;
+
+namespace Planilla_Backend.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly string _connectionString;
+        public UserRepository()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _connectionString = builder.Configuration.GetConnectionString("OnigiriContext");
+        }
+
+        public async Task<User?> GetActiveEmailAsync(string correo)
+        {
+            const string sql = @" SELECT u.IdUsuario, u.Correo, u.Contrasena, u.Estado, u.IdPersona,
+                                  p.Cedula, p.IdPersona, p.Nombre1, p.Nombre2, p.Apellido1, p.Apellido2, p.Telefono, p.FechaNacimiento, p.TipoPersona
+                                  FROM dbo.Usuario u
+                                  JOIN dbo.Persona p ON p.IdPersona = u.IdPersona
+                                  WHERE u.Correo = @Correo AND u.Estado = 'Activo';";
+
+            using var conn = new SqlConnection(_connectionString);
+
+            User? usuario = null;
+            await conn.QueryAsync<User, Person, User>(
+                sql,
+                (u, p) =>
+                {
+                    if (usuario == null)
+                    {
+                        usuario = u;
+                    }
+                    usuario.Persona = p;
+                    return usuario;
+                },
+                new { Correo = correo },
+                splitOn: "Cedula"
+            );
+
+            return usuario;
+        }
+    }
+}
