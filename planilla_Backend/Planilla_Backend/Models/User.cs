609aa44e64b717a5de8ae5cd92b255971e84dd8d
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-20 11:49:53 -0600

    remover default controller, cambios a archivos del proyecto

---
+++ planilla_Backend/Planilla_Backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Planilla_Backend.Models
+{
+    public class User
+    {
+        public int IdUsuario { get; set; }
+        public string Correo { get; set; } = "";
+        public string Contrasena { get; set; } = "";  // texto plano por ahora
+        public string Estado { get; set; } = "";      // 'Activo' | 'Inactivo'
+        public int IdPersona { get; set; }
+        public Person? Persona { get; set; }
+    }
+}
