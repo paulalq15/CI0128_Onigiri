6826b4d1fd448961967f05c618e3612605397dcc
N.Lin <70311238+namlin@users.noreply.github.com>
2025-09-20 18:06:59 -0600

    Implementación del método GET de la API y del middleware para leer tokens.

---
+++ API_Pensiones_Voluntarias/Controllers/API_Controller.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PensionAPI.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]  // URL: https://localhost:7019/api/controller
+
+    public class API_Controller : ControllerBase {
+        [HttpGet]
+        public IActionResult Get(char? planType, double? grossSalary) {
+            try {
+                // Validate the sent parameters:
+                if (planType == null || grossSalary == null) {
+                    return BadRequest(new { error = "Bad Request: Missing parameters" });  // 400
+                }
+
+                if (double.IsNaN(grossSalary.Value)) {
+                    return BadRequest(new { error = "Bad Request: 'parameter grossSalary' must be a number" });  // 400
+                }
+
+                if (grossSalary <= 0) {
+                    return BadRequest(new { error = "Bad Request: 'parameter grossSalary' must be a positive decimal" });  // 400
+                }
+
+                double salary = grossSalary.Value;
+                double employerContribution = 0;
+                double employeeContribution = 0;
+
+                switch (planType) {
+                    case 'A':
+                        employerContribution = salary * 0.035;
+                        employeeContribution = salary * 0.03;
+                        break;
+
+                    case 'B':
+                        employerContribution = salary * 0.055;
+                        employeeContribution = salary * 0.05;
+                        break;
+
+                    case 'C':
+                        employerContribution = salary * 0;
+                        employeeContribution = salary * 0.01;
+                        break;
+
+                    default:
+                        return BadRequest(new { error = "Bad Request: Parameter 'planType' can only be A, B or C" });  // 400
+                }
+
+                // 200:
+                return Ok(new
+                {
+                    PlanType = planType,
+                    GrossSalary = grossSalary,
+                    EmployerContribution = employerContribution,
+                    EmployeeContribution = employeeContribution,
+                });
+            }
+            
+            catch (Exception ex) {
+                return StatusCode(500, new { error = "Internal Server Error", details = ex.Message });
+            }
+        }
+    }
+}
--- API_Pensiones_Voluntarias/Program.cs
+++ API_Pensiones_Voluntarias/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,12 +14,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//------------------------------------------------------------------------------------------------
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +27,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Middleware to validate tokens sent by the API clients:
+app.Use(async (context, next) => {
+    // This apply onlt if there are /api routes:
+    if (context.Request.Path.StartsWithSegments("/api")) {
+        // Get the defined token in the settings file:
+        var tokenConfig = builder.Configuration["TokenConfig:Secret"];
+
+        // Verify if the client sent the authorization header:
+        if (!context.Request.Headers.TryGetValue("Authorization", out var token) ||
+            token != $"Bearer {tokenConfig}") {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsJsonAsync(new { error = "Forbidden: Missing or invalid token" });
+            return;
+        }
+    }
+
+    await next();
+});
