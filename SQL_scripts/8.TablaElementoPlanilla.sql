84cc1c28c19e8261616c4cd7f48ba3a1b3b97382
paulalq15 <paulalq15@gmail.com>
2025-09-22 15:24:30 -0600

    Agregando query para crear la tabla de elemento de planilla

---
+++ SQL_scripts/8.TablaElementoPlanilla.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE ElementoPlanilla(
+IdElemento INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+Nombre CHAR(40) NOT NULL,
+PagadoPor VARCHAR(8) NOT NULL,
+Tipo VARCHAR(10) NOT NULL,
+Valor DECIMAL(10,2) NOT NULL,
+Estado VARCHAR(8) NOT NULL DEFAULT 'Activo',
+CONSTRAINT CK_ElementoPagadoPor CHECK (PagadoPor IN ('Empleado','Empleador')),
+CONSTRAINT CK_TipoCalculo CHECK (Tipo IN ('Monto','Porcentaje','API')),
+CONSTRAINT CK_EstadoElemento CHECK (Estado IN ('Activo','Inactivo'))
+)
+GO
