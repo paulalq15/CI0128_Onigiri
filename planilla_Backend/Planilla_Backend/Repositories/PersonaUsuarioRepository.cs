3bd5915d0c01b15740bd806a65cff6a15ec56956
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-21 21:19:28 -0600

    Metodos necesarios para guardar el registro de un nuevo usuario empleador

---
+++ planilla_Backend/Planilla_Backend/Repositories/PersonaUsuarioRepository.cs
@@ -0,0 +1,107 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Planilla_Backend.Models;
+
+namespace Planilla_Backend.Repositories
+{
+  public class PersonaUsuarioRepository
+  {
+    private readonly string _connectionString;
+    public PersonaUsuarioRepository()
+    {
+      var builder = WebApplication.CreateBuilder();
+      _connectionString = builder.Configuration.GetConnectionString("OnigiriContext");
+    }
+
+    // Método para guardar una nueva Persona y su Usuario en la base de datos
+    public int savePersonaAndUsuario(PersonaUsuario persona, string password)
+    {
+      using var connection = new SqlConnection(_connectionString);
+      connection.Open();
+
+      // Usamos una transacción para asegurarar que si falla la inserción de una tabla, no se inserte la otra
+      using var transaction = connection.BeginTransaction();
+
+      try
+      {
+        // Insertar Persona
+        var sqlPersona = @"INSERT INTO Persona
+            (Cedula, Nombre1, Nombre2, Apellido1, Apellido2, Telefono, FechaNacimiento, TipoPersona)
+            VALUES
+            (@Cedula, @Nombre1, @Nombre2, @Apellido1, @Apellido2, @Telefono, @FechaNacimiento, @TipoPersona);
+            SELECT CAST(SCOPE_IDENTITY() as int);";
+
+        var idPersona = connection.QuerySingle<int>(sqlPersona, persona, transaction);
+        // Insertar Usuario
+        var sqlUsuario = @"INSERT INTO Usuario
+            (Correo, Contrasena, Estado, IdPersona)
+            VALUES
+            (@Correo, @password, @Estado, @IdPersona);
+            SELECT CAST(SCOPE_IDENTITY() as int);";
+
+        var idUsuario = connection.QuerySingle<int>(sqlUsuario, new { persona.Correo, password, persona.Estado, IdPersona = idPersona }, transaction);
+
+        transaction.Commit();
+        return idUsuario;
+      }
+      catch (Exception ex)
+      {
+        // Si falla, revertimos la transacción y retornamos un error
+        transaction.Rollback();
+        Console.WriteLine(ex.ToString());
+        throw new Exception("Error al guardar Persona y Usuario: " + ex.Message);
+      }
+    }
+
+    // Método para obtener una Persona&Usuario por correo y contraseña. ? permite retornar null
+    public PersonaUsuario? GetPersonaUsuarioByCredentials(string correo, string password)
+    {
+      using var connection = new SqlConnection(_connectionString);
+
+      PersonaUsuario? resultPersona = null;
+
+      try
+      {
+        // Obtener Usuario y Persona asociada
+        var sql = "SELECT u.IdUsuario, u.Correo, u.Estado, u.IdPersona, " +
+              "p.* " +
+              "FROM Usuario u " +
+              "INNER JOIN Persona p ON u.IdPersona = p.IdPersona " +
+              "WHERE u.Correo = @Correo AND u.Contrasena = @Password";
+
+
+        resultPersona = connection.QueryFirstOrDefault<PersonaUsuario>(sql, new { Correo = correo, Password = password });
+      }
+      catch (Exception ex) {
+        throw new Exception("Error al obtener usuario" + ex.Message);
+      }
+
+      return resultPersona;
+    }
+
+    // Método para obtener el IdPersona por correo
+    public int getUserIdByEmail(string email)
+    {
+      using var connection = new SqlConnection(_connectionString);
+
+      int resultUserdID = 0;
+
+      try
+      {
+        // Obtener Usuario
+        var sql = @"SELECT u.IdUsuario
+              FROM Usuario u
+              WHERE u.Correo = @Correo";
+
+        resultUserdID = connection.QueryFirstOrDefault<int>(sql, new { Correo = email });
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Error al obtener usuario" + ex.Message);
+      }
+
+      // Si no existe el correo, devuelve 0
+      return resultUserdID;
+    }
+  }
+}
---
+++ planilla_Backend/Planilla_Backend/Services/PersonaUsuarioService.cs
@@ -0,0 +1,78 @@
+﻿using Planilla_Backend.Models;
+using Planilla_Backend.Repositories;
+
+namespace Planilla_Backend.Services
+{
+  public class PersonaUsuarioService
+  {
+    private readonly PersonaUsuarioRepository personaUsuarioRepository;
+    private readonly DirectionsRepository directionsRepository;
+
+    public PersonaUsuarioService()
+    {
+      personaUsuarioRepository = new PersonaUsuarioRepository();
+      directionsRepository = new DirectionsRepository();
+    }
+
+    // Servicio para guardar una nueva Persona y su Usuario
+    public int savePersonaUsuario(PersonaUsuario persona, string password)
+    {
+      try
+      {
+        int idUsuario = personaUsuarioRepository.savePersonaAndUsuario(persona, password);
+        return idUsuario;
+      } catch (Exception) {
+        // Retornamos un valor de error (-1)
+        return -1;
+      }
+    }
+
+    // Servicio para obtener una Persona&Usuario por correo y contraseña
+    public PersonaUsuario? GetPersonaUsuario(string email, string password)
+    {
+      try
+      {
+        PersonaUsuario? personaUsuario = personaUsuarioRepository.GetPersonaUsuarioByCredentials(email, password);
+        return personaUsuario;
+      } catch (Exception) {
+        // Retornar null en caso de error
+        return null;
+      }
+    }
+
+    // Servicio para guardar la dirección de una persona
+    public int savePersonaDirection(int idPersona, string zipCode , string otherSigns)
+    {
+      // Obtener el id de la división territorial
+      int idDivision = directionsRepository.getDivisionByZipCode(zipCode);
+
+      if (idDivision != -1)
+      {
+        try
+        {
+          // Obtener el id de direccion
+          int idDirection = directionsRepository.saveDirection(idDivision, otherSigns, null, idPersona);
+          return idDirection;
+        } catch (Exception) {
+          // Retornar -1 en caso de error al insertar
+          return -1;
+        }
+      }
+
+      // Retornar -1 si no se encontró el zip code
+      return -1;
+    }
+
+    public int getUserIdByEmail(string email)
+    {
+      try
+      {
+        int idPersona = personaUsuarioRepository.getUserIdByEmail(email);
+        return idPersona;
+      } catch (Exception) {
+        // Retornar -1 en caso de error
+        return -1;
+      }
+    }
+  }
+}
