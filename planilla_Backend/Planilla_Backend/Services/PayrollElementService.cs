c27e5b6984b6ea7b890c678e3a0b53c52624b7f9
paulalq15 <paulalq15@gmail.com>
2025-09-22 15:55:43 -0600

    Agregando el service para la creación de elemento de planilla

--- planilla_Backend/Planilla_Backend/Services/PayrollElementService.cs
+++ planilla_Backend/Planilla_Backend/Services/PayrollElementService.cs
@@ -11,5 +11,24 @@ namespace Planilla_Backend.Services
         {
             payrollElementRepository = new PayrollElementRepository();
         }
+
+        public string CreatePayrollElement(PayrollElementModel element)
+        {
+            var result = string.Empty;
+            try
+            {
+                var isCreated = payrollElementRepository.CreatePayrollElement(element);
+                if (!isCreated)
+                {
+                    result = "Error al crear el elemento de planilla";
+                }
+            }
+            catch (Exception ex)
+            {
+                result = $"Error creando elemento de planilla: {ex.Message}";
+            }
+
+            return result;
+        }
     }
 }
