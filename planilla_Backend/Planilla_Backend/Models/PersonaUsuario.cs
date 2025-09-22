30c02eb5b092a1161cc6ff296167ae9da49cb5f4
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-21 21:18:13 -0600

    valor tipoPersona en el modelo PersonaUsuario

---
+++ planilla_Backend/Planilla_Backend/Models/PersonaUsuario.cs
@@ -0,0 +1,19 @@
+﻿// Tabla Persona & Usuario
+namespace Planilla_Backend.Models
+{
+  public class PersonaUsuario
+  {
+    public int IdUsuario { get; set; }
+    public string Correo { get; set; }
+    public string Estado { get; set; }
+    public int IdPersona { get; set; }
+    public string Cedula { get; set; }
+    public string Nombre1 { get; set; }
+    public string Nombre2 { get; set; }
+    public string Apellido1 { get; set; }
+    public string Apellido2 { get; set; }
+    public string Telefono { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string TipoPersona { get; set; }
+  }
+}
