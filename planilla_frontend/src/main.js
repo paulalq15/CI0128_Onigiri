9040bb8ac62c15048712ee99820a60446f2adacd
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-23 16:10:57 -0600

    Agrear configuracion para vue router

--- planilla_frontend/src/main.js
+++ planilla_frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginForm from './components/LoginForm.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: "/", name: "Home", component: LoginForm},
+    ]
+
+});
 
 createApp(App).mount('#app')
