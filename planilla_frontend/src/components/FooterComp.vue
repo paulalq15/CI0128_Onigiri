d1f306bfd2d675a4d81074509ce8056ecb0fae4a
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-23 19:18:24 -0600

    actualizaciones al header y al footer

--- planilla_frontend/src/components/FooterComp.vue
+++ planilla_frontend/src/components/FooterComp.vue
@@ -1,15 +1,10 @@
 <template>
     <footer class="py-3 my-4"> 
-        <p class="text-center text-white fs-5">© 2025 Todos los derechos reservados - Onigiri Intelligent Solutions</p> 
+        <p class="text-center text-white">© 2025 Todos los derechos reservados - Onigiri Intelligent Solutions</p> 
     </footer>
 </template>
 
 <script>
-    export default {
-        setup() {
-            return {}
-        },
-    }
 </script>
 
 <style lang="scss" scoped>
