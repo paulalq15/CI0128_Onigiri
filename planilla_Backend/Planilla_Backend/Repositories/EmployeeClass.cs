6f9c45240a4b2207ac64e8ab3e452f711f1f2c52
N.Lin <230016456+josue-badilla@users.noreply.github.com>
2025-09-24 15:21:41 -0600

    Archivos base del modelo y repositorio del modelo Employee.

---
+++ planilla_Backend/Planilla_Backend/Repositories/EmployeeClass.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Planilla_Backend.Models;
+using System.Data.SqlClient;
+
+namespace Planilla_Backend.Repositories 
+{
+    public class EmployeeRepository
+    {
+        private readonly string _connectionString;
+
+        public EmployeeRepository()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _connectionString = builder.Configuration.GetConnectionString("?");
+        }
+
+        public List<EmployeeModel> GetUsers()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string query = "SELECT * FROM dbo.Usuario";
+            return connection.Query<EmployeeModel>(query).ToList();
+        }
+    }
+}
