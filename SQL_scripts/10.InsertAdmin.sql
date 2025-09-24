b11cd9a24f63ac438e5907409c84eff747aa7ddc
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-23 23:17:01 -0600

    Actializaciones SQL

--- SQL_scripts/10.InsertAdmin.sql
+++
@@ -1,7 +0,0 @@
-﻿-- Insertar en Persona
-INSERT INTO Persona (Cedula, Nombre1, Nombre2, Apellido1, Apellido2, Telefono, FechaNacimiento, TipoPersona)
-VALUES ('12345678901', 'Angel', 'Jesus', 'Mena', 'Coudin', '88887777', '2004-05-12', 'Administrador');
-
--- Insertar en Usuario (usamos el IdPersona recién creado)
-INSERT INTO Usuario (Correo, Contrasena, Estado, IdPersona)
-VALUES ('admin@example', 'Admin123', 'Activo', SCOPE_IDENTITY());
