0a0104700e43f979aa759c5669bd24f1a123b422
paulalq15 <paulalq15@gmail.com>
2025-09-25 12:07:45 -0600

    Agregando detalles al sidebar para que se cierre al dar click en una opcion, y si se abre una categoría, se cierren las otras

--- planilla_frontend/src/components/SidebarComp.vue
+++ planilla_frontend/src/components/SidebarComp.vue
@@ -12,36 +12,36 @@
                 <span>Paula Lopez</span>
             </div>
 
-            <ul class="list-unstyled ps-0"> 
+            <ul class="list-unstyled ps-0" id="sidebar-accordion"> 
                 <li class="border-top my-3"></li>
                 <!--Para volver a la página principal-->
                 <li class="my-3">
-                    <RouterLink class="link-body-emphasis text-decoration-none" to="/Home" @click="closeSidebar">
-                        <i class="bi bi-house me-3"></i>
-                        <span>Página principal</span>
+                    <RouterLink class="link-body-emphasis text-decoration-none" to="/Home">
+                        <div class="gap-2 px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">
+                            <i class="bi bi-house me-3"></i>
+                            <span>Página principal</span>
+                        </div>
                     </RouterLink>
                 </li>
                 <!--Opciones para empresa-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#company-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#company-collapse" aria-expanded="false" aria-controls="company-collapse">
                         <i class="bi bi-buildings me-3"></i>
                         <span class="flex-grow-1 text-start">Empresas</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="company-collapse">
+                    <div class="collapse" id="company-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Ver Empresas
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Ver Empresas</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/CrearEmpresa" @click="closeSidebar">Crear Empresa
+                                <RouterLink class="nav-link my-3" to="/CrearEmpresa">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear Empresa</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -50,24 +50,22 @@
                 <!--Opciones para empleados-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#employee-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#employee-collapse" aria-expanded="false" aria-controls="employee-collapse">
                         <i class="bi bi-people me-3"></i>
                         <span class="flex-grow-1 text-start">Empleados</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="employee-collapse">
+                    <div class="collapse" id="employee-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Ver Empleados
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Ver Empleados</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear Empleados
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear Empleados</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -76,30 +74,27 @@
                 <!--Opciones para timesheets-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#timesheet-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#timesheet-collapse" aria-expanded="false" aria-controls="timesheet-collapse">
                         <i class="bi bi-calendar-week me-3"></i>
                         <span class="flex-grow-1 text-start">Timesheets</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="timesheet-collapse">
+                    <div class="collapse" id="timesheet-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Ver Timesheets
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Ver Timesheets</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Aprobar Timesheets
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Aprobar Timesheets</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear Timesheet
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear Timesheet</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -108,48 +103,42 @@
                 <!--Opciones para planilla-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#payroll-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#payroll-collapse" aria-expanded="false" aria-controls="payroll-collapse">
                         <i class="bi bi-calculator me-3"></i>
                         <span class="flex-grow-1 text-start">Planilla</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="payroll-collapse">
+                    <div class="collapse" id="payroll-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Historial de planillas
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Historial de planillas</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear planilla
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear planilla</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Ver beneficios y deducciones
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Ver beneficios y deducciones</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear beneficios y deducciones
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear beneficios y deducciones</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Asignar deducciones
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Asignar deducciones</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Seleccionar beneficios
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Seleccionar beneficios</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -158,24 +147,22 @@
                 <!--Opciones para pagos-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#payment-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#payment-collapse" aria-expanded="false" aria-controls="payment-collapse">
                         <i class="bi bi-credit-card me-3"></i>
                         <span class="flex-grow-1 text-start">Pagos</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="payment-collapse">
+                    <div class="collapse" id="payment-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Historial de pagos
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Historial de pagos</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear pago
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear pago</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -184,24 +171,22 @@
                 <!--Opciones para reportes-->
                 <li class="my-3">
                     <button
-                        class="nav-link d-flex w-100"
-                        data-bs-toggle="collapse" data-bs-target="#report-collapse" aria-expanded="false">
+                        class="nav-link d-flex w-100 gap-2 px-2 py-2 sidebar-item collapsed"
+                        data-bs-toggle="collapse" data-bs-target="#report-collapse" aria-expanded="false" aria-controls="report-collapse">
                         <i class="bi bi-bar-chart me-3"></i>
                         <span class="flex-grow-1 text-start">Reportes</span>
                         <i class="bi bi-chevron-right caret"></i>
                     </button>
-                    <div class="collapse" id="report-collapse">
+                    <div class="collapse" id="report-collapse" data-bs-parent="#sidebar-accordion">
                         <ul class="list-unstyled ms-4 ps-2 small">
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Ver Reportes
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Ver Reportes</span>
                                 </RouterLink>
                             </li>
                             <li>
-                                <RouterLink
-                                    class="nav-link my-2"
-                                    to="/Home" @click="closeSidebar">Crear Reporte
+                                <RouterLink class="nav-link my-3" to="/Home">
+                                    <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear Reporte</span>
                                 </RouterLink>
                             </li>
                         </ul>
@@ -210,9 +195,11 @@
                 <li class="border-top my-3"></li> 
                 <!--Botón para cerrar sesión en rojo-->    
                 <li class="my-3">
-                    <RouterLink class="nav-link link-danger text-decoration-none" to="/" @click="closeSidebar">
-                        <i class="bi bi-box-arrow-right me-3"></i>
-                        <span>Cerrar sesión</span>
+                    <RouterLink class="nav-link link-danger" to="/">
+                        <div class="gap-2 px-2 py-2" data-bs-dismiss="offcanvas">
+                            <i class="bi bi-box-arrow-right me-3"></i>
+                            <span>Cerrar sesión</span>
+                        </div>
                     </RouterLink>
                 </li>
             </ul>
@@ -224,4 +211,12 @@
 </script>
 
 <style lang="scss" scoped>
+
+    .sidebar-item:hover{
+        border-radius: .5rem;
+        background-color: rgba(35,77,52,.12);
+        color: #1b3d2a;
+        cursor: pointer;
+    }
+
 </style>
