9d2e090b7c839df94e196c7d3e43d9da9c3af8e4
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-28 14:27:40 -0600

    Lógica para el envío de correos

--- planilla_Backend/Planilla_Backend/Controllers/PersonUserController.cs
+++ planilla_Backend/Planilla_Backend/Controllers/PersonUserController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Planilla_Backend.Models;
 using Planilla_Backend.Services;
-using System.Text.Json;
+using Planilla_Backend.Services.Email.EmailModels;
+using Planilla_Backend.Services.Email.EmailTypes;
+using Planilla_Backend.Services.EmailService;
+using Planilla_Backend.Services.Utils;
 
 namespace Planilla_Backend.Controllers
 {
@@ -11,10 +13,14 @@ namespace Planilla_Backend.Controllers
   public class PersonUserController : ControllerBase
   {
     private readonly PersonUserService personUserService;
+    private readonly IEmailService emailService;
+    private readonly Utils utils;
 
-    public PersonUserController()
+    public PersonUserController(IEmailService emailService, Utils utils)
     {
       personUserService = new PersonUserService();
+      this.emailService = emailService;
+      this.utils = utils;
     }
 
     [HttpPost("register")]
@@ -29,7 +35,26 @@ namespace Planilla_Backend.Controllers
       if (idPerson != -1) {
         int idDirection = personUserService.savePersonDirection(idPerson, registerData.zipCode, registerData.otherSigns);
         if (idDirection != -1) {
-          return Created(string.Empty, idPerson);
+
+          // Enviar correo de activación
+          ActivationAccountModel activationModel = new ActivationAccountModel();
+          ActivationAccountEmail activationAccountEmail = new ActivationAccountEmail();
+          activationModel.userName = registerData.personData.Name1 + " " + registerData.personData.Surname1;
+
+          //Generar token de activación
+          string token = this.utils.GenerateJWToken(idPerson);
+
+          activationModel.activationLink = $"https://localhost:7071/api/Tokens/ActivateAccount?token={token}";
+
+          // Enviar correo
+          try
+          {
+            emailService.SendEmail(registerData.personData.Email, activationAccountEmail.GetSubject(), activationAccountEmail.GetBody(activationModel));
+          } catch (Exception)
+          {
+            return BadRequest("Error al enviar el correo de activación");
+          }
+          return Ok(idPerson);
         }
       }
 
