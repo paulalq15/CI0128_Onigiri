7ff7c8823c5efc7875821842fdb02ec2ca2d1d67
N.Lin <driscollucs@gmail.com>
2025-09-28 03:00:40 -0600

    Componente Vue.js para la página de ver empresas completado.

--- planilla_frontend/src/components/ViewCompanies.vue
+++ planilla_frontend/src/components/ViewCompanies.vue
@@ -1,15 +1,66 @@
 <template>
-  <!--Restrict page if user is Employee or Approver-->
-  <div
-    v-if="$session.user?.typeUser === 'Empleado' || $session.user?.typeUser === 'Aprobador'"
-    class="d-flex flex-column text-center"
-  >
-    <h3>Acceso restringido</h3>
-    <p>Esta página no está disponible</p>
-  </div>
-  <div v-else class="d-flex flex-column"></div>
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Lista de Empresas</h1>
+
+        <div class="row justify-content-end">
+            <div class="col-2"></div>
+        </div>
+
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+            <tr>
+                <th>Cédula Jurídica</th>
+                <th>Nombre</th>
+                <th>Teléfono</th>
+                <th>Cantidad de Beneficios</th>
+                <th>Frecuencia de Pago</th>
+                <th>Día de Pago #1</th>
+                <th>Día de Pago #2</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="(company, index) of companies" :key="index">
+                <td>{{ company.companyId }}</td>
+                <td>{{ company.companyName }}</td>
+                <td>{{ company.telephone }}</td>
+                <td>{{ company.maxBenefits }}</td>
+                <td>{{ company.paymentFrequency }}</td>
+                <td>{{ company.payDay1 }}</td>
+                <td>{{ company.payDay2 }}</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+    export default {
+        name: "CompaniesList",
+
+        data() {
+            return {
+                companies: [],
+            };
+        },
+
+        methods: {
+            getCompanies() {
+                axios.get("https://localhost:7071/api/Company/getCompanies").then((response) => {
+                this.companies = response.data;
+
+                }).catch(error => {
+                    console.error("Error al obtener compañías:", error);
+                });
+            },
+        },
+
+        created: function () {
+            this.getCompanies();
+        },
+    };
+
+</script>
 
 <style lang="scss" scoped></style>
