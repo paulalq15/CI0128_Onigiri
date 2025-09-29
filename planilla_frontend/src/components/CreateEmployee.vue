8eb64eb624f17ae62e77747963e8de87f0b23a66
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-28 18:23:31 -0600

    completar script para registrar empleado

--- planilla_frontend/src/components/CreateEmployee.vue
+++ planilla_frontend/src/components/CreateEmployee.vue
@@ -5,7 +5,7 @@
     <p>Esta página no está disponible</p>
   </div>
 
-  <div class="d-flex flex-column" id="container">
+  <div v-else class="d-flex flex-column" id="container">
     <!--Cuerpo-->
     <div class="d-flex justify-content-center align-items-center">
       <!--Formulario-->
@@ -16,55 +16,55 @@
       >
         <h2 class="font-weigth-normal text-center mb-4">Registrar un empleado</h2>
         <!--Primer nombre-->
-        <InputType label="Primer nombre" id="firstName" v-model="Employee.Name1" />
-        <span class="text-danger" v-if="errors.Name1">{{ errors.Name1 }}</span>
+        <InputType label="Primer nombre" id="firstName" v-model="Employee.name1" />
+        <span class="text-danger" v-if="errors.name1">{{ errors.name1 }}</span>
         <!--Segundo nombre-->
         <InputType
           label="Segundo nombre"
           id="SecondName"
-          v-model="Employee.Name2"
+          v-model="Employee.name2"
           :required="false"
         />
         <!--Primer apellido-->
-        <InputType label="Primer apellido" id="firstSurname" v-model="Employee.Surname1" />
-        <span class="text-danger" v-if="errors.Surname1">{{ errors.Surname1 }}</span>
+        <InputType label="Primer apellido" id="firstSurname" v-model="Employee.surname1" />
+        <span class="text-danger" v-if="errors.surname1">{{ errors.surname1 }}</span>
         <!--Segundo apellido-->
-        <InputType label="Segundo apellido" id="secondSurname" v-model="Employee.Surname2" />
-        <span class="text-danger" v-if="errors.Surname2">{{ errors.Surname2 }}</span>
+        <InputType label="Segundo apellido" id="secondSurname" v-model="Employee.surname2" />
+        <span class="text-danger" v-if="errors.surname2">{{ errors.surname2 }}</span>
         <!--Cédula-->
         <InputType
           label="Cédula de indentidad"
           id="IDCard"
           placeHolder="0-0000-0000"
           :max_length="11"
-          v-model="Employee.IdCard"
+          v-model="Employee.idCard"
         />
-        <span class="text-danger" v-if="errors.IdCard">{{ errors.IdCard }}</span>
+        <span class="text-danger" v-if="errors.idCard">{{ errors.idCard }}</span>
         <!--Fecha nacimiento-->
         <InputType
           label="Fecha de nacimiento"
           id="birthday"
           type="date"
-          v-model="Employee.BirthdayDate"
+          v-model="Employee.birthDate"
         />
-        <span class="text-danger" v-if="errors.BirthdayDate">{{ errors.BirthdayDate }}</span>
+        <span class="text-danger" v-if="errors.birthDate">{{ errors.birthDate }}</span>
         <!--Correo electrónico-->
         <InputType
           label="Correo electrónico"
           id="email"
           type="email"
           placeHolder="name@example.com"
-          v-model="Employee.Email"
+          v-model="Employee.email"
         />
-        <span class="text-danger" v-if="errors.Email">{{ errors.Email }}</span>
+        <span class="text-danger" v-if="errors.email">{{ errors.email }}</span>
         <!--Puesto-->
-        <InputType label="Puesto" id="position" v-model="Contract.Position" />
-        <span class="text-danger" v-if="errors.Position">{{ errors.Position }}</span>
+        <InputType label="Puesto" id="position" v-model="Contract.position" />
+        <span class="text-danger" v-if="errors.position">{{ errors.position }}</span>
         <!--Departamento-->
         <SelectType
           label="Departamento"
           id="department"
-          v-model="Contract.Department"
+          v-model="Contract.department"
           :options="[
             { value: 'Administracion', text: 'Administración' },
             { value: 'Contabilidad', text: 'Contabilidad' },
@@ -75,45 +75,45 @@
             { value: 'Ventas', text: 'Ventas' }
           ]"
         />
-        <span class="text-danger" v-if="errors.Department">{{ errors.Department }}</span>
+        <span class="text-danger" v-if="errors.department">{{ errors.department }}</span>
         <!--Salario-->
         <InputType
           label="Salario"
           id="salary"
           type="number"
-          v-model="Contract.Salary"
+          v-model="Contract.salary"
         />
-        <span class="text-danger" v-if="errors.Salary">{{ errors.Salary }}</span>
+        <span class="text-danger" v-if="errors.salary">{{ errors.salary }}</span>
         <!--Tipo de empleado-->
         <SelectType
           label="Tipo de empleado"
           id="employeeType"
-          v-model="Contract.EmployeeType"
+          v-model="Contract.employeeType"
           :options="[
             { value: 'Tiempo Completo', text: 'Tiempo Completo' },
             { value: 'Medio Tiempo', text: 'Medio Tiempo' },
             { value: 'Servicios Profesionales', text: 'Servicios Profesionales' }
           ]"
         />
-        <span class="text-danger" v-if="errors.EmployeeType">{{ errors.EmployeeType }}</span>
+        <span class="text-danger" v-if="errors.employeeType">{{ errors.employeeType }}</span>
         <!--Fecha de inicio-->
         <InputType
           label="Fecha de inicio"
           id="startDate"
           type="date"
-          v-model="Contract.StartDate"
+          v-model="Contract.startDate"
         />
-        <span class="text-danger" v-if="errors.StartDate">{{ errors.StartDate }}</span>
+        <span class="text-danger" v-if="errors.startDate">{{ errors.startDate }}</span>
         <!--Cuenta bancaria IBAN-->
         <InputType
           label="Cuenta bancaria IBAN"
           id="iban"
-          v-model="Contract.BankAccount"
+          v-model="Contract.bankAccount"
         />
-        <span class="text-danger" v-if="errors.BankAccount">{{ errors.BankAccount }}</span>
+        <span class="text-danger" v-if="errors.bankAccount">{{ errors.bankAccount }}</span>
         <!--Botón confirmar-->
         <div class="form-group text-center">
-          <LinkButton type="button" @click="saveRegisterData" text="Registrar" />
+          <LinkButton type="button" @click="saveEmployee" text="Guardar Empleado" />
         </div>
       </form>
     </div>
@@ -123,8 +123,6 @@
 <script>
 // Alerta global en App.vue
 import { useGlobalAlert } from '@/utils/alerts.js';
-
-import { toRaw } from 'vue';
 
 // Base URL de la API
 import URLBaseAPI from '../axiosAPIInstances.js';
@@ -146,79 +144,71 @@ export default {
       errors: {},
 
       Employee: {
-        Email: '',
-        IdCard: '',
-        Name1: '',
-        Name2: '',
-        Surname1: '',
-        Surname2: '',
-        Number: '',
-        BirthdayDate: '',
-        TypePerson: 'Empleado',
-        Status: 'Inactivo',
-        Password: 'Temporary01!'
+        idCard: '',
+        name1: '',
+        name2: '',
+        surname1: '',
+        surname2: '',
+        phone: '9999-9999',
+        birthDate: '',
+        personType: 'Empleado',
+        email: '',
+        password: 'Temporary01!',
+        status: 'Inactivo',
+        companyUniqueId: ''
       },
 
       Contract: {
-        Position: '',
-        Department: '',
-        Salary: '',
-        EmployeeType: '',
-        StartDate: '',
-        BankAccount: '',
+        position: '',
+        department: '',
+        salary: '',
+        employeeType: '',
+        startDate: '',
+        bankAccount: '',
       },
-
     };
   },
 
   methods: {
-
     // Validar los datos del formulario, agregar errores al objeto errors
     async validateForm() {
       this.errors = {};
 
       // Nombre y apellidos
-      if (!this.Persona.Name1) this.errors.Name1 = 'El primer nombre es obligatorio';
-      if (!this.Persona.Surname1) this.errors.Surname1 = 'El primer apellido es obligatorio';
-      if (!this.Persona.Surname2) this.errors.Surname2 = 'El segundo apellido es obligatorio';
+      if (!this.Employee.name1) this.errors.name1 = 'El primer nombre es obligatorio';
+      if (!this.Employee.surname1) this.errors.surname1 = 'El primer apellido es obligatorio';
+      if (!this.Employee.surname2) this.errors.surname2 = 'El segundo apellido es obligatorio';
 
       // Cédula
-      if (!this.Persona.IdCard) this.errors.IdCard = 'La cedula es obligatorio';
-      else if (!/^\d{1}-\d{4}-\d{4}$/.test(this.Persona.IdCard))
-        this.errors.IdCard = 'El formato debe ser #-####-####, solo valores numéricos';
-
-      // Teléfono
-      if (!this.Persona.Number) this.errors.Number = 'El teléfono es obligatorio';
-      else if (this.Persona.Number.length < 8)
-        this.errors.Number = 'El teléfono debe tener al menos 8 caracteres';
-      else if (!/^\d{4}-\d{4}$/.test(this.Persona.Number))
-        this.errors.Number = 'El formato debe ser ####-####, solo valores numéricos';
+      if (!this.Employee.idCard) this.errors.idCard = 'La cedula es obligatorio';
+      else if (!/^\d{1}-\d{4}-\d{4}$/.test(this.Employee.idCard))
+        this.errors.idCard = 'El formato debe ser #-####-####, solo valores numéricos';
 
       // Fecha de nacimiento
-      if (!this.Persona.BirthdayDate)
-        this.errors.BirthdayDate = 'La fecha de nacimiento es obligatoria';
+      if (!this.Employee.birthDate)
+        this.errors.birthDate = 'La fecha de nacimiento es obligatoria';
       else {
         // Obtener fecha actual
         const today = new Date();
         // Convertir en objeto Date
-        const birthDate = new Date(this.Persona.BirthdayDate);
+        const birthDate = new Date(this.Employee.birthDate);
         // Calcular la edad obteniendo los años
         var age = today.getFullYear() - birthDate.getFullYear();
 
-        if (age < 18) this.errors.BirthdayDate = 'Debe ser mayor de edad para registrarse';
+        if (age < 18) this.errors.birthDate = 'Debe ser mayor de edad para registrarse';
       }
 
       // Correo
-      if (!this.Persona.Email) this.errors.Email = 'El correo es obligatorio';
-      else if (!/\S+@\S+\.\S+/.test(this.Persona.Email)) this.errors.Email = 'Correo inválido';
+      if (!this.Employee.email) this.errors.email = 'El correo es obligatorio';
+      else if (!/\S+@\S+\.\S+/.test(this.Employee.email)) this.errors.email = 'Correo inválido';
       else {
         // Esperar la verificación de correo
         try {
           const response = await URLBaseAPI.get('/api/PersonUser/emailCheck', {
-            params: { email: this.Persona.Email },
+            params: { email: this.Employee.email },
           });
           if (response.data > 0) {
-            this.errors.Email = 'El correo ya está en uso';
+            this.errors.email = 'El correo ya está en uso';
           }
         } catch (error) {
           console.error(error);
@@ -229,21 +219,39 @@ export default {
     },
 
     // Enviar datos del formulario para guardar
-    async saveRegisterData() {
+    async saveEmployee() {
       if (!(await this.validateForm())) {
         return;
       }
 
-      const register = {
-        personData: toRaw(this.Employee),
-        //password: this.password,
-        //otherSigns: this.Direccion.otherSigns,
-        //zipCode: this.zipCode.value,
+      const payload = {
+        personData: {
+          cedula: this.Employee.idCard,
+          name1: this.Employee.name1,
+          name2: this.Employee.name2 || null,
+          surname1: this.Employee.surname1,
+          surname2: this.Employee.surname2 || '',
+          phone: this.Employee.phone,
+          birthDate: this.Employee.birthDate,
+          personType: this.Employee.personType,
+        },
+        userData: {
+          email: this.Employee.email,
+          password: this.Employee.password,
+          status: this.Employee.status
+        },
+        contractData: {
+          position: this.Contract.position,
+          department: this.Contract.department,
+          salary: Number(this.Contract.salary),
+          employeeType: this.Contract.employeeType,
+          startDate: this.Contract.startDate,
+          bankAccount: this.Contract.bankAccount
+        },
+        companyUniqueId: Number(this.$session.user?.companyUniqueId)
       };
 
-      console.log(register);
-
-      URLBaseAPI.post('/api/PersonUser/register', register)
+      URLBaseAPI.post('/api/Employee', payload)
         .then((response) => {
           console.log('OK:', response.data);
 
@@ -251,14 +259,12 @@ export default {
           const alert = useGlobalAlert();
 
           alert.show(
-            'Registro exitoso. Por favor, active su cuenta mediante el correo de activación enviado a ' +
-              this.Persona.Email +
-              ' para poder iniciar sesión',
+            'Registro exitoso.',
             'success'
           );
 
           // Redireccionamiento que permite mantener la alerta
-          this.$router.push('/');
+          this.$router.push('/home');
         })
         .catch((error) => {
           if (error.response) console.log('Error del backend:', error.response.data);
