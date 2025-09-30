edd37dba5110a3174efc858816495dad53f925a3
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-29 22:51:53 -0600

    agregar funcionalidad de enviar email al controller de empleado

--- planilla_Backend/Planilla_Backend/Controllers/EmployeeController.cs
+++ planilla_Backend/Planilla_Backend/Controllers/EmployeeController.cs
@@ -1,16 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Planilla_Backend.Models;
 using Planilla_Backend.Services;
+using Planilla_Backend.Services.Email.EmailModels;
+using Planilla_Backend.Services.Email.EmailTypes;
+using Planilla_Backend.Services.EmailService;
+using Planilla_Backend.Services.Utils;
 
 [Route("api/[controller]")]
 [ApiController]
 public class EmployeeController : ControllerBase
 {
   private readonly EmployeeService _employeeService;
+  private readonly IEmailService _emailService;
+  private readonly Utils _tokenUtil;
 
-  public EmployeeController(EmployeeService employeeService)
+  public EmployeeController(EmployeeService employeeService, IEmailService emailService, Utils tokenUtil)
   {
     _employeeService = employeeService;
+    _emailService = emailService;
+    _tokenUtil = tokenUtil;
   }
 
   [HttpPost]
@@ -18,6 +26,29 @@ public class EmployeeController : ControllerBase
   {
     // Front-end already validated; here we just call the service
     int personId = _employeeService.RegisterEmployee(employee);
-    return Ok(new { success = true, personId });
+
+
+    if (personId != -1)
+    {
+      ActivationAccountModel activationModel = new ActivationAccountModel();
+      activationModel.userName = employee.PersonData.Name1 + " " + employee.PersonData.Surname1;
+      EmployeeActivationEmail activationAccountEmail = new EmployeeActivationEmail(activationModel);
+
+      //Generar token de activación
+      string token = this._tokenUtil.GenerateJWToken(personId);
+
+      activationModel.activationLink = $"https://localhost:7071/api/Tokens/ActivateEmployee?token={token}";
+
+      // Enviar correo
+      try
+      {
+        _emailService.SendEmail(employee.UserData.Email, activationAccountEmail.GetSubject(), activationAccountEmail.GetBody(activationModel));
+      } catch (Exception)
+      {
+        return BadRequest("Error al enviar el correo de activación");
+      }
+      return Ok(new { success = true, personId });
+    }
+    return BadRequest("Error al registrar al empleado");
   }
 }
