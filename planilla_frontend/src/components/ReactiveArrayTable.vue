eaaa31597f42ee818fe09632a6956c1735a4f067
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-30 15:19:08 -0600

    Frontend para mostrar los elementos de planilla al administrador según la empresa que seleccione

--- planilla_frontend/src/components/ReactiveArrayTable.vue
+++ planilla_frontend/src/components/ReactiveArrayTable.vue
@@ -1,26 +1,52 @@
 <template>
-  <div class="container p-5 pt-4 pb-4 mt-5" style="background-color: white; border-radius: 20px;">
-    <h3 class="display-5 text-center mb-4">{{ title }}</h3>
+  <div class="container p-5">
+    <h3 class="display-5 text-center mb-3">{{ title }}</h3>
 
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th v-for="(head, index) in tableHeader" :key="index" scope="col">
-            {{ head.label }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(element, index) in tableElements" :key="index">
-          <th scope="row">{{ index + 1 }}</th>
-          <td v-for="(head, j) in tableHeader" :key="j">
-            {{ element[head.key] }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <!-- Filtro empresa -->
+    <div v-if="isAdmin" class="form-group mb-3">
+      <label for="companyFilter">Empresa:</label>
+      <select
+        v-model="selectedCompanyLocal"
+        class="form-select"
+        id="companyFilter"
+        @change="$emit('filter-company', selectedCompanyLocal)"
+        style="width: 35%;">
+        <option value="" disabled>Seleccione una empresa</option>
+        <option
+          v-for="company in companies"
+          :key="company.companyUniqueId"
+          :value="company.companyUniqueId">
+          {{ company.companyName }}
+        </option>
+      </select>
+    </div>
 
+    <!-- Tabla -->
+    <div class="table-responsive p-3"  style="background-color: white; border-radius: 10px;">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th v-for="(head, index) in tableHeader" :key="index" scope="col">
+              {{ head.label }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-for="(element, index) in tableElements" :key="index">
+            <tr>
+              <td>{{ index + 1 }}</td>
+              <td v-for="(head, j) in tableHeader" :key="j">
+                {{ element[head.key] }}
+              </td>
+            </tr>
+            <tr v-if="index < tableElements.length - 1" class="spacer">
+              <td :colspan="tableHeader.length + 1"></td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -28,24 +54,29 @@
 export default {
   name: "ReactiveObjectTable",
   props: {
-    title: {
-      type: String,
-      default: "Tabla"
-    },
-    tableHeader: {
-      type: Array,
-      required: true
-    },
-    tableElements: {
-      type: Array,
-      required: true
+    title: { type: String, default: "Tabla" },
+    tableHeader: { type: Array, required: true },
+    tableElements: { type: Array, required: true },
+    companies: { type: Array, default: () => [] },
+    isAdmin: { type: Boolean, default: false },
+    selectedCompany: { type: String, default: ""}
+  },
+
+  data() {
+    return {
+      selectedCompanyLocal: this.selectedCompany
+    };
+  },
+  
+  watch: {
+    selectedCompany(newVal) {
+      this.selectedCompanyLocal = newVal;
     }
   }
 };
 </script>
 
 <style scoped>
-
   table.table td {
     border: none;
   }
--- planilla_frontend/src/components/ViewPayrollElement.vue
+++ planilla_frontend/src/components/ViewPayrollElement.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="d-flex flex-column" id="container">
     <!--Cuerpo-->
-    <div class="d-flex justify-content-center align-items-center">
-      <ReactiveObjectTable :title="title" :tableHeader="header" :tableElements="elements"/> 
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <ReactiveObjectTable
+        :title="title"
+        :tableHeader="header"
+        :tableElements="elements"
+        :companies="companies"
+        :isAdmin="isAdmin"
+        :selectedCompany="selectedCompany"
+        @filter-company="GetCompanyPayrollElements"
+      /> 
     </div>
   </div>
 </template>
@@ -17,14 +25,18 @@
   import ReactiveObjectTable from './ReactiveArrayTable.vue';
 
   const  {user} = useSession();
+  var isAdmin = ref(false);
+  var companies = ref([]);
+  var selectedCompany = ref("");
 
   const title = "Elementos de planilla"
   const header = [
-    { label: "ELEMENTO", key: "elementName" },
-    { label: "TIPO", key: "calculationType" },
-    { label: "VALOR", key: "calculationValue" },
-    { label: "PAGADO POR", key: "paidBy" },
-    { label: "ESTADO", key: "status"}
+    { label: "Elemento", key: "elementName" },
+    { label: "Monto/Porcentaje", key: "calculationType" },
+    { label: "Valor", key: "calculationValue" },
+    { label: "Tipo", key: "type"},
+    { label: "Pagado por", key: "paidBy" },
+    { label: "Estado", key: "status"}
   ];
   var elements = ref([]);
 
@@ -32,13 +44,36 @@
     GetPayrollElements();
   });
 
-  async function GetPayrollElements() {
-    const companyId = user.companyUniqueId;
-    console.log("Company id: " + companyId)
-    await URLBaseAPI.get("/api/PayrollElement/GetPayRollElements", {params: {idCompany: companyId}})
+  function GetPayrollElements() {
+    const userType = user.typeUser;
+    console.log("Tipo de usuario: " + userType);
+    if (userType === "Administrador") GetCompanies();
+    else {
+      const companyId = user.companyUniqueId;
+      GetCompanyPayrollElements(companyId);
+    }
+  }
+
+  async function GetCompanies() {
+    isAdmin.value = true;
+    await URLBaseAPI.get("/api/Company/GetAllCompaniesSummary")
+        .then((response) => {
+          companies.value = response.data;
+          console.log("Empresas: ", companies.value);
+        })
+        .catch();
+  }
+
+  async function GetCompanyPayrollElements(idCompany) {
+    console.log("Company id: " + idCompany);
+    await URLBaseAPI.get("/api/PayrollElement/GetPayRollElements", {params: {idCompany: idCompany}})
         .then((response) => {
           console.log(response.data);
-          elements.value = response.data; // sin mapear, todo queda tal cual
+          elements.value = response.data.map(e => ({
+            ...e,
+            type: e.paidBy === 'Empleador' ? 'Beneficio' : 'Deducción'
+          }));
+          elements.value.paidBy == elements.value.calculationType === 'API' ? 'Empleador' : elements.value.paidBy;
         })
         .catch((error) => {
           if (error.response) console.log('Error del backend:', error.response.data);
