324e2180dc01880127b2d1149d8089a39c62551d
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-30 16:24:05 -0600

    agregando filtro por tipo de elemento y sacando los filtros de la pantalla ReactiveArrayTable para hacerla más reutilizable

--- planilla_frontend/src/components/ReactiveArrayTable.vue
+++ planilla_frontend/src/components/ReactiveArrayTable.vue
@@ -1,52 +1,28 @@
 <template>
-  <div class="container p-5">
-    <h3 class="display-5 text-center mb-3">{{ title }}</h3>
-
-    <!-- Filtro empresa -->
-    <div v-if="isAdmin" class="form-group mb-3">
-      <label for="companyFilter">Empresa:</label>
-      <select
-        v-model="selectedCompanyLocal"
-        class="form-select"
-        id="companyFilter"
-        @change="$emit('filter-company', selectedCompanyLocal)"
-        style="width: 35%;">
-        <option value="" disabled>Seleccione una empresa</option>
-        <option
-          v-for="company in companies"
-          :key="company.companyUniqueId"
-          :value="company.companyUniqueId">
-          {{ company.companyName }}
-        </option>
-      </select>
-    </div>
-
-    <!-- Tabla -->
-    <div class="table-responsive p-3"  style="background-color: white; border-radius: 10px;">
-      <table class="table">
-        <thead>
+  <div class="table-responsive pt-3 pb-3 ps-4 pe-4" style="background-color: white; border-radius: 10px;">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th v-for="(head, index) in tableHeader" :key="index" scope="col">
+            {{ head.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(element, index) in tableElements" :key="index">
           <tr>
-            <th scope="col">#</th>
-            <th v-for="(head, index) in tableHeader" :key="index" scope="col">
-              {{ head.label }}
-            </th>
+            <td>{{ index + 1 }}</td>
+            <td v-for="(head, j) in tableHeader" :key="j">
+              {{ element[head.key] }}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          <template v-for="(element, index) in tableElements" :key="index">
-            <tr>
-              <td>{{ index + 1 }}</td>
-              <td v-for="(head, j) in tableHeader" :key="j">
-                {{ element[head.key] }}
-              </td>
-            </tr>
-            <tr v-if="index < tableElements.length - 1" class="spacer">
-              <td :colspan="tableHeader.length + 1"></td>
-            </tr>
-          </template>
-        </tbody>
-      </table>
-    </div>
+          <tr v-if="index < tableElements.length - 1" class="spacer">
+            <td :colspan="tableHeader.length + 1"></td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -54,25 +30,9 @@
 export default {
   name: "ReactiveObjectTable",
   props: {
-    title: { type: String, default: "Tabla" },
     tableHeader: { type: Array, required: true },
-    tableElements: { type: Array, required: true },
-    companies: { type: Array, default: () => [] },
-    isAdmin: { type: Boolean, default: false },
-    selectedCompany: { type: String, default: ""}
+    tableElements: { type: Array, required: true }
   },
-
-  data() {
-    return {
-      selectedCompanyLocal: this.selectedCompany
-    };
-  },
-  
-  watch: {
-    selectedCompany(newVal) {
-      this.selectedCompanyLocal = newVal;
-    }
-  }
 };
 </script>
 
--- planilla_frontend/src/components/ViewPayrollElement.vue
+++ planilla_frontend/src/components/ViewPayrollElement.vue
@@ -1,22 +1,52 @@
 <template>
-  <div class="d-flex flex-column" id="container">
-    <!--Cuerpo-->
-    <div class="d-flex flex-column justify-content-center align-items-center">
+  <div class="d-flex justify-content-center p-5 ms-5 me-5">
+    <div class="w-100 ps-5 pe-5 ms-4 me-4">
+      <h3 class="display-5 text-center mb-4">Elementos de planilla</h3>
+      <!--Filtros-->
+      <div class="d-flex mb-4 gap-3">
+        <!-- Filtro empresa (solo admin) -->
+        <div v-if="isAdmin" class="form-group">
+          <label for="companyFilter">Empresa:</label>
+          <select
+            v-model="selectedCompany"
+            class="form-select"
+            id="companyFilter"
+            @change="GetCompanyPayrollElements(selectedCompany)">
+            <option value="" disabled>Seleccione una empresa</option>
+            <option
+              v-for="company in companies"
+              :key="company.companyUniqueId"
+              :value="company.companyUniqueId">
+              {{ company.companyName }}
+            </option>
+          </select>
+        </div>
+
+        <!-- Filtro tipo deducción o beneficio -->
+        <div class="form-group">
+          <label for="typeElementFilter">Tipo de elemento:</label>
+          <select
+            v-model="selectedElementType"
+            class="form-select"
+            id="typeElementFilter">
+            <option value="" disabled>Seleccione un elemento</option>
+            <option value="Todos">Todos</option>
+            <option value="Beneficio">Beneficio</option>
+            <option value="Deducción">Deducción</option>
+          </select>
+        </div>
+      </div>
+      <!--tabla-->
       <ReactiveObjectTable
-        :title="title"
         :tableHeader="header"
-        :tableElements="elements"
-        :companies="companies"
-        :isAdmin="isAdmin"
-        :selectedCompany="selectedCompany"
-        @filter-company="GetCompanyPayrollElements"
-      /> 
+        :tableElements="filteredElements"
+      />     
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
 
   // Base URL de la API
   import URLBaseAPI from '../axiosAPIInstances.js';
@@ -28,8 +58,13 @@
   var isAdmin = ref(false);
   var companies = ref([]);
   var selectedCompany = ref("");
+  var selectedElementType = ref("Todos");
 
-  const title = "Elementos de planilla"
+  const filteredElements = computed(() => {
+    if (selectedElementType.value === "Todos") return elements.value;
+    return elements.value.filter(el => el.type === selectedElementType.value);
+  })
+
   const header = [
     { label: "Elemento", key: "elementName" },
     { label: "Monto/Porcentaje", key: "calculationType" },
