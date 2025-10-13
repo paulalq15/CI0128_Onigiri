f014081f0f8168eccd3deab29ce1b51c954c99ab
paulalq15 <paulalq15@gmail.com>
2025-10-12 18:04:48 -0600

    Agregando los SQL Scripts necesarios para la creación y pago de planilla

---
+++ SQL_scripts/14.TablaImpuestoRenta.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE ImpuestoRenta(
+	IdImpuestoRenta INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	FechaInicio DATE NOT NULL,
+	FechaFin DATE NOT NULL,
+	Porcentaje Decimal(5,2) NOT NULL,
+	MontoMinimo INT NOT NULL,
+	MontoMaximo INT
+);
