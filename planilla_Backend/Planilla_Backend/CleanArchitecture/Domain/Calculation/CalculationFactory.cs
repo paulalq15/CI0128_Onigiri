01ec9f51db28dca6aa69eeee01e864f62487b821
paulalq15 <paulalq15@gmail.com>
2025-10-15 16:41:01 -0600

    Agregando los dos posibles casos del tipo de salario en el calculation factory para llamarlo desde el payroll template

--- planilla_Backend/Planilla_Backend/CleanArchitecture/Domain/Calculation/CalculationFactory.cs
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Domain/Calculation/CalculationFactory.cs
@@ -4,16 +4,29 @@ namespace Planilla_Backend.CleanArchitecture.Domain.Calculation
 {
   public class CalculationFactory
   {
+    private readonly Salary_FixedStrategy _fixedStrategy;
+    private readonly Salary_HourlyStrategy _hourlyStrategy;
+
+    public CalculationFactory(Salary_FixedStrategy fixedStrategy, Salary_HourlyStrategy hourlyStrategy)
+    {
+      _fixedStrategy = fixedStrategy ?? throw new ArgumentNullException(nameof(fixedStrategy));
+      _hourlyStrategy = hourlyStrategy ?? throw new ArgumentNullException(nameof(hourlyStrategy));
+    }
     public ISalaryBaseStrategy CreateBaseStrategy(ContractModel contract, CompanyModel company)
     {
-      // TODO: create instance and return the correct base salary strategy
-      throw new NotImplementedException();
+      if (contract == null) throw new ArgumentNullException(nameof(contract));
+
+      if (_hourlyStrategy.Applicable(contract)) return (ISalaryBaseStrategy)_hourlyStrategy;
+      if (_fixedStrategy.Applicable(contract)) return (ISalaryBaseStrategy)_fixedStrategy;
+
+      throw new InvalidOperationException("No base salary strategy found for the contract type.");
+
     }
 
     public IList<IConceptStrategy> CreateConceptStrategies(CompanyModel company, string period)
     {
       // TODO: create and return the list of concept strategies to apply
-      throw new NotImplementedException();
+      return new List<IConceptStrategy>();
     }
   }
 }
