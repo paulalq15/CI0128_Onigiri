c1640e44178e3d7cf1275712ab8e48c84f7740df
paulalq15 <paulalq15@gmail.com>
2025-10-14 22:38:50 -0600

    Agregando la estructura del repositorio con los posibles queries y commands necesarios para crear toda la planilla y pago de planilla

---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/Ports/IPayrollRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Planilla_Backend.CleanArchitecture.Application.Ports
+{
+  using Planilla_Backend.CleanArchitecture.Domain.Entities;
+  public interface IPayrollRepository
+  {
+    // Queries
+    CompanyModel GetCompany(int companyId);
+    IEnumerable<EmployeeModel> GetEmployees(int companyId);
+    IEnumerable<ContractModel> GetContracts(int companyId, DateOnly DateFrom, DateOnly DateTo);
+    IEnumerable<ElementModel> GetElementsForEmployee(int companyId, int employeeId, DateOnly dateFrom, DateOnly dateTo);
+    IEnumerable<TaxModel> GetTaxes(int companyId, DateOnly dateFrom, DateOnly dateTo);
+    IEnumerable<CCSSModel> GetCCSS(int companyId, DateOnly dateFrom, DateOnly dateTo);
+
+    // Commands
+    int SaveCompanyPayroll(CompanyPayrollModel header);
+    int SaveEmployeePayroll(EmployeePayrollModel employeePayroll);
+    void SavePayrollDetails(int employeePayrollId, IEnumerable<PayrollDetailModel> details);
+    void UpdateEmployeePayrollTotals(int employeePayrollId, EmployeePayrollModel totalsAndStatus);
+    void UpdateCompanyPayrollTotals(int companyPayrollId, CompanyPayrollModel totalsAndStatus);
+    void SavePayment(int employeePayrollId, PaymentModel payment);
+  }
+}
---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Infrastructure/PayrollRepository.cs
@@ -0,0 +1,73 @@
+﻿using Planilla_Backend.CleanArchitecture.Application.Ports;
+using Planilla_Backend.CleanArchitecture.Domain.Entities;
+
+namespace Planilla_Backend.CleanArchitecture.Infrastructure
+{
+  public class PayrollRepository : IPayrollRepository
+  {
+    public PayrollRepository()
+    {
+      //Inject dependencies
+    }
+
+    // =======================
+    //  TODO: READ METHODS
+    // =======================
+
+    public CompanyModel GetCompany(int companyId)
+    {
+      return new CompanyModel();
+    }
+
+    public IEnumerable<EmployeeModel> GetEmployees(int companyId)
+    {
+      return new List<EmployeeModel>();
+    }
+
+    public IEnumerable<ContractModel> GetContracts(int companyId, DateOnly DateFrom, DateOnly DateTo)
+    {
+      return new List<ContractModel>();
+    }
+
+    public IEnumerable<ElementModel> GetElementsForEmployee(int companyId, int employeeId, DateOnly dateFrom, DateOnly dateTo)
+    {
+      return new List<ElementModel>();
+    }
+
+    public IEnumerable<TaxModel> GetTaxes(int companyId, DateOnly dateFrom, DateOnly dateTo)
+    {
+      return new List<TaxModel>();
+    }
+
+    public IEnumerable<CCSSModel> GetCCSS(int companyId, DateOnly dateFrom, DateOnly dateTo)
+    {
+      return new List<CCSSModel>();
+    }
+
+    // =======================
+    //  TODO: WRITE METHODS
+    // =======================
+
+    public int SaveCompanyPayroll(CompanyPayrollModel header)
+    {
+      return 0;
+    }
+    public int SaveEmployeePayroll(EmployeePayrollModel employeePayroll)
+    {
+      return 0;
+    }
+    public void SavePayrollDetails(int employeePayrollId, IEnumerable<PayrollDetailModel> details)
+    {
+    }
+    public void UpdateEmployeePayrollTotals(int employeePayrollId, EmployeePayrollModel totalsAndStatus)
+    {
+    }
+    public void UpdateCompanyPayrollTotals(int companyPayrollId, CompanyPayrollModel totalsAndStatus)
+    {
+    }
+    public void SavePayment(int employeePayrollId, PaymentModel payment)
+    {
+    }
+
+  }
+}
