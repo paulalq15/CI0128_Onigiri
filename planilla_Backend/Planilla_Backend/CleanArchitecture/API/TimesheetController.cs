dbfb2236988e62dff1df95086e5f53827bfc9eda
Anabelle <anabelle.porras@ucr.ac.cr>
2025-10-15 22:34:00 -0600

    API para guardar las horas de trabajo de empleados

---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/API/TimesheetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Planilla_Backend.CleanArchitecture.Application.Services;
+using Planilla_Backend.CleanArchitecture.Application.UseCases;
+using Planilla_Backend.LayeredArchitecture.Models;
+using System.Security.Claims;
+
+namespace Planilla_Backend.CleanArchitecture.API
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TimesheetController : ControllerBase
+  {
+    private readonly ITimesheetService _svc;
+    public TimesheetController(ITimesheetService svc) => _svc = svc;
+
+    [HttpPost("week/{personId:int}")]
+    public async Task<IActionResult> SaveWeek([FromRoute] int personId, [FromBody] SaveWeekHoursRequest req, CancellationToken ct)
+    {
+      var employeeId = personId;
+      await _svc.InsertWeekAsync(employeeId, req.WeekStart, req.Entries, ct);
+      return NoContent();
+    }
+  }
+}
