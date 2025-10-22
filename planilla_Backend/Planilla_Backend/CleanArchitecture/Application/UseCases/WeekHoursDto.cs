4ba60d86584f74ba50dab66ec70b1de6ce7adfcd
Anabelle <anabelle.porras@ucr.ac.cr>
2025-10-21 19:39:20 -0600

    nuevo DTO para almacenar la semana y horas

---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/UseCases/WeekHoursDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Planilla_Backend.CleanArchitecture.Application.UseCases
+{
+  public class WeekHoursDto
+  {
+    public DateTime WeekStart { get; set; }
+    public DateTime WeekEnd { get; set; }
+    //public List<DateTime> Days { get; set; } = new();
+    public List<DayEntryDto> Entries { get; set; } = new();
+  }
+}
