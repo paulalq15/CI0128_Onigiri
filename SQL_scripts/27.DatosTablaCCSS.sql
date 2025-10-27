76248f1d4c5125601e38966fd129e1051ece3164
AngDM01 <hopro64@gmail.com>
2025-10-26 18:52:32 -0600

    Datos de la tabla actualizados al nuevo formato de la columna porcentaje

---
+++ SQL_scripts/27.DatosTablaCCSS.sql
@@ -0,0 +1,15 @@
+INSERT INTO CCSS (FechaInicio, Porcentaje, Categoria, Concepto, PagadoPor)
+VALUES
+    ('2025-01-01', 0.0925, 'CCSS', 'SEM', 'Empleador'),
+    ('2025-01-01', 0.0550, 'CCSS', 'SEM', 'Empleado'),
+    ('2025-01-01', 0.0542, 'CCSS', 'IVM', 'Empleador'),
+    ('2025-01-01', 0.0417, 'CCSS', 'IVM', 'Empleado'),
+    ('2025-01-01', 0.0025, 'Otras instituciones', 'Banco Popular (BPOP)', 'Empleador'),
+    ('2025-01-01', 0.0500, 'Otras instituciones', 'Asignaciones familiares', 'Empleador'),
+    ('2025-01-01', 0.0050, 'Otras instituciones', 'IMAS', 'Empleador'),
+    ('2025-01-01', 0.0150, 'Otras instituciones', 'INA', 'Empleador'),
+    ('2025-01-01', 0.0025, 'Ley de Protección al Trabajador (LPT)', 'Banco Popular (BPOP)', 'Empleador'),
+    ('2025-01-01', 0.0100, 'Ley de Protección al Trabajador (LPT)', 'Banco Popular (BPOP)', 'Empleado'),
+    ('2025-01-01', 0.0150, 'Ley de Protección al Trabajador (LPT)', 'Fondo de Capitalización Laboral (FCL)', 'Empleador'),
+    ('2025-01-01', 0.0200, 'Ley de Protección al Trabajador (LPT)', 'Operadora de pensiones complementaria (OPC)', 'Empleador'),
+    ('2025-01-01', 0.0100, 'Ley de Protección al Trabajador (LPT)', 'INS', 'Empleador');
