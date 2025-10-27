a1f5482e9381a7f955971a68abcd8e4a1a0fa320
Anabelle <anabelle.porras@ucr.ac.cr>
2025-10-27 15:32:52 -0600

    Cambio a EmployeeModel para soportar modificaciones al empleado

---
+++ planilla_Backend/Planilla_Backend/LayeredArchitecture/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+using Planilla_Backend.Models;
+
+namespace Planilla_Backend.LayeredArchitecture.Models
+{
+  public class EmployeeModel
+  {
+    public Person? PersonData { get; set; }
+    public User? UserData { get; set; }
+    public Contract? ContractData { get; set; }
+    public int CompanyUniqueId { get; set; }
+
+    //Address
+    public DirectionsModel? Directions { get; set; }
+    public string? AddressDetails { get; set; }
+    public string? ZipCode { get; set; }
+  }
+}
