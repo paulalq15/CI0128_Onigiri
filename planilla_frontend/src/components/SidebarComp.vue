5f817392eead444ed4148aba026d350cf8ba00bb
Anabelle <anabelle.porras@ucr.ac.cr>
2025-10-28 09:07:52 -0600

    modificar opciones de empleado

--- planilla_frontend/src/components/SidebarComp.vue
+++ planilla_frontend/src/components/SidebarComp.vue
@@ -100,9 +100,14 @@
                   <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Crear Empleados</span>
                 </RouterLink>
               </li>
-              <li>
+              <li v-if="session.user?.typeUser === 'Empleador'">
                 <RouterLink class="nav-link my-3" to="/app/Empleados/ModificarEmpleado">
                   <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Modificar Empleado</span>
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink class="nav-link my-3" to="/app/Empleados/ModificarPerfil">
+                  <span class="px-2 py-2 sidebar-item" data-bs-dismiss="offcanvas">Modificar Mi Perfil</span>
                 </RouterLink>
               </li>
             </ul>
