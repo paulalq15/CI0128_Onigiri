c393ba478c69734bd620d32e4cba48b37d422d43
Anabelle <anabelle.porras@ucr.ac.cr>
2025-10-27 21:48:14 -0600

    actualizacion para usar DirectionModel

--- planilla_Backend/Planilla_Backend/LayeredArchitecture/Models/EmployeeModel.cs
+++ planilla_Backend/Planilla_Backend/LayeredArchitecture/Models/EmployeeModel.cs
@@ -10,8 +10,6 @@ namespace Planilla_Backend.LayeredArchitecture.Models
     public int CompanyUniqueId { get; set; }
 
     //Address
-    public DirectionsModel? Directions { get; set; }
-    public string? AddressDetails { get; set; }
-    public string? ZipCode { get; set; }
+    public DirectionsModel? Direction { get; set; }
   }
 }
