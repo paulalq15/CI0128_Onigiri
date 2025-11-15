72b794d0a00ea35f0b62a4032f7cf994b83acf59
N.Lin <driscollucs@gmail.com>
2025-11-15 12:31:40 -0600

    Función para exportar reportes a PDFs empleando la bilbioteca jspdf.

--- planilla_frontend/src/components/reports/EmployerReportDetailPayroll.vue
+++ planilla_frontend/src/components/reports/EmployerReportDetailPayroll.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>
+  <div id="report" ref="reportContainer">
     Reporte detallado del pago de planilla (Empleador, PLAN-110)
   </div>
+
+  <button class="btn btn-primary" @click="exportToPDF">Exportar a PDF</button>
 </template>
 
-<script setup>
+<script>
+  import jsPDF from "jspdf";
+
+  export default {
+    data() {
+      return {
+      };
+    },
+
+    methods: {
+      exportToPDF() {
+        const doc = new jsPDF();
+        const element = this.$refs.reportContainer;
+      
+        doc.html(element, {
+          callback: function (pdf) {
+            pdf.save("EmployerReportDetailPayroll.pdf");
+          },
+
+          x: 10,
+          y: 10,
+          width: 190,
+          windowWidth: element.scrollWidth,
+        });
+      }
+    }
+  }
 
 </script>
 
