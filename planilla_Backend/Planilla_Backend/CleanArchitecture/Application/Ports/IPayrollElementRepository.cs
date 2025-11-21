726fea8291df0f49c92f2a1332f564245b707eaf
Anabelle <anabelle.porras@ucr.ac.cr>
2025-11-21 17:28:30 -0600

    nuevo metodo para eliminar elemnto de planilla

--- planilla_Backend/Planilla_Backend/CleanArchitecture/Application/Ports/IPayrollElementRepository.cs
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/Ports/IPayrollElementRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Planilla_Backend.CleanArchitecture.Domain.Entities;
+using Planilla_Backend.CleanArchitecture.Domain.Reports;
 
 namespace Planilla_Backend.CleanArchitecture.Application.Ports
 {
   public interface IPayrollElementRepository
   {
     Task<PayrollElementEntity?> GetPayrollElementByElementId(int elementId);
-
     Task<int> UpdatePayrollElement(PayrollElementEntity payrollElement);
+    Task<IEnumerable<DeletePayrollElementEmailDto>> DeletePayrollElement(int elementId);
   }
 }
