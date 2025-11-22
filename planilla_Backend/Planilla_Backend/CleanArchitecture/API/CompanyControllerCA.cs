cd7c6510743dc916e2d902aca352ed4317acf0df
AngDM01 <hopro64@gmail.com>
2025-11-21 23:18:30 -0600

    Avance. Lógica del backend para la eliminación de una empresa, con lógica de seguridad y manejo de excepciones

---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/API/CompanyControllerCA.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planilla_Backend.CleanArchitecture.Application.UseCases.company;
+
+namespace Planilla_Backend.CleanArchitecture.API
+{
+  [ApiController]
+  [Route("api/external")]
+  public class CompanyControllerCA : ControllerBase
+  {
+    private readonly IDeleteCompanyCommand deleteCompanyCommand;
+
+    public CompanyControllerCA(IDeleteCompanyCommand deleteCompanyCommand)
+    {
+      this.deleteCompanyCommand = deleteCompanyCommand;
+    }
+
+    [HttpDelete("deleteCompany")]
+    public async Task<ActionResult<int>> deleteCompany(int companyId, int employeerPersonId)
+    {
+      int rowsAffected = await this.deleteCompanyCommand.Execute(companyId, employeerPersonId);
+      return Ok(rowsAffected);
+    }
+  }
+}
---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/UseCases/company/DeleteCompanyCommand.cs
@@ -0,0 +1,33 @@
+﻿using Planilla_Backend.CleanArchitecture.Application.Ports;
+
+namespace Planilla_Backend.CleanArchitecture.Application.UseCases.company
+{
+  public class DeleteCompanyCommand : IDeleteCompanyCommand
+  {
+    private readonly ICompanyRepository companyRepository;
+
+    public DeleteCompanyCommand(ICompanyRepository createCompanyRepository)
+    {
+      this.companyRepository = createCompanyRepository;
+    }
+
+    public async Task<int> Execute(int companyUniqueId, int personId)
+    {
+      if (personId < 0) throw new ArgumentException("Id de persona inválido");
+      if (companyUniqueId < 0) throw new ArgumentException("Id de la empresa inválido");
+
+      int isAdmin = await this.companyRepository.IsPersonAdmin(personId);
+
+      if (isAdmin != 1)
+      {
+        int isOwner = await this.companyRepository.IsPersonOwnerOfCompany(companyUniqueId, personId);
+
+        if (isOwner != 1) throw new KeyNotFoundException("No tiene la autorización para realizar esta acción");
+      }
+
+      int rowsAffected = await this.companyRepository.DeleteCompanyByUniqueId(companyUniqueId);
+
+      return rowsAffected;
+    }
+  }
+}
