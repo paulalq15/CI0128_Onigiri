25bae6d693d7f0cc95bf861c8ec72f83ce73f80a
AngDM01 <hopro64@gmail.com>
2025-11-23 19:08:00 -0600

    Lógica para crear un correo de eliminación

---
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/EmailsDTOs/DeleteCompanyEmployeeDataDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Planilla_Backend.CleanArchitecture.Application.EmailsDTOs
+{
+  public class DeleteCompanyEmployeeDataDTO
+  {
+    public string EmployeeEmail { get; set; }
+    public string UserName { get; set; }
+    public string CompanyName { get; set; }
+  }
+}
