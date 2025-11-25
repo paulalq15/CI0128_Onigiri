004f1ae3a57d4d3ad2e60f9d13a85396c38931b5
Anabelle <anabelle.porras@ucr.ac.cr>
2025-11-24 20:54:15 -0600

    actualizacion de interfaz

--- planilla_Backend/Planilla_Backend/CleanArchitecture/Application/Ports/IDashboardRepository.cs
+++ planilla_Backend/Planilla_Backend/CleanArchitecture/Application/Ports/IDashboardRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Planilla_Backend.CleanArchitecture.Application.Ports
+﻿using Planilla_Backend.CleanArchitecture.Domain.Entities;
+
+namespace Planilla_Backend.CleanArchitecture.Application.Ports
 {
   public interface IDashboardRepository
   {
+    Task<List<EmployerDashboardCostByTypeModel>> GetEmployerDashboardCostByType(int companyId);
+    Task<List<EmployerDashboardCostByMonthModel>> GetEmployerDashboardCostByMonth(int companyId);
+    Task<List<EmployerDashboardEmployeeCountByTypeModel>> GetEmployerDashboardEmployeeCountByType(int companyId);
   }
 }
